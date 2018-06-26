8f73aa12355fde675f39590112b484b36f797c17
abadug5793 <bavani7777@gmail.com>
2018-06-26 14:15:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,11 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/name' do 
+  get '/name' do
     "My name is Avani"
   end
 
-  
+  get '/hometown' do
+    "My hometown is Ellicott City"
+  end
 
+  get '/favorite-song' do
+    "My favorite-song is nothing, because I don't have a favorite"
+  end
 
 end
